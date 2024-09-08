e6aa18224f44a6b2e114f532952360324aa93eb1
RobertKonenske <konenskerobert@gmail.com>
2024-09-08 15:45:34 -0400

    added images and linked images for json

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -3,3 +3,18 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     alert('Form submitted successfully!');
     // Here you can add code to handle the form submission, e.g., sending the data to a server
 });
+
+async function getData() {
+    const url = "travel_reccomendation_api.json";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
